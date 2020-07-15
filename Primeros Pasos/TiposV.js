4fc1df152da0c20c7a81a963f4f7402c6febe8e4
unknown <francoorellana1@gmail.com>
2020-07-15 20:53:08 -0300

    Introduccion a JS

---
+++ Primeros Pasos/TiposV.js
@@ -0,0 +1,19 @@
+var nombre = "Franco" //string
+console.log(nombre)
+var miNumero = 23 //number
+console.log(miNumero)
+const PI = 3.14 //decimal
+console.log(PI)
+var verdadero = true //boolean true
+var falso = false //boolean false
+console.log(verdadero)
+console.log(falso)
+var nulo = null //null
+console.log(nulo)
+var indefinido = undefined //indefinido
+console.log(indefinido)
+
+console.log(typeof nombre) //el atributo "typeof" informa que tipo de variable es
+
+const E = Math.E
+console.log(E)
---
+++ Primeros Pasos/variables.js
@@ -0,0 +1,18 @@
+//var, variables globales
+var saludo="Hola Mundo"
+if(saludo){
+    var saludo = "Hola"
+    console.log(saludo)
+}
+console.log(saludo)
+
+//let, variables limitadas al bloque donde se encuentran definidas
+let nombre = "Hello"
+console.log(nombre)
+if(nombre){
+    let nombre = "CHAU"
+    console.log(nombre)
+}
+//const, variables inmutables
+const PI = 3.14
+console.log(PI)
