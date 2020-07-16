4cfd296a4bfb8d400f9689ff3c033c217f28ba61
unknown <francoorellana1@gmail.com>
2020-07-15 23:35:03 -0300

    Introduccion a JS-Operadores

---
+++ Primeros Pasos/operadores.js
@@ -0,0 +1,9 @@
+//OPERADORES: https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+//operador ternario: condicion ? true : false
+
+var edad = 20
+var res = edad >=18 ? console.log("Es mayor de edad") : console.log("Es menor de edad")
+
+console.log(res)
+
---
+++ Primeros Pasos/string.js
@@ -0,0 +1,11 @@
+var nombre = "Franco"
+var edad = 28 
+
+console.log(`Mi nombre es: ${nombre} y tengo ${edad} años`) //concatenar strings
+
+console.log(nombre.toUpperCase()) //convertir todo el string a mayuscula
+console.log(nombre.toLowerCase()) //idem a minuscula
+console.log(nombre.length) // Q de caracteres
+
+//TODOS LOS METODOS DE STRINGS EN: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String
+
